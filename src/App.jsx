83a6d233a1d880dfbfe1393f39f0846dec976b1a
Blacklion567 <naviedaj567@gmail.com>
2023-12-23 21:42:22 +0800

    React-The-Complete-Guide-2023-incl.-React-Router-Redux

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import FirstApp from './components/Section-01-Getting-Started First-App/FirstApp';
+import SectionThree from "./components/Section-03-React-Essentials-Components-JSX-Props-State-and-More/SectionThree";
+
+
+const App = () => {
+  return (
+    <>
+      {/* <FirstApp /> */}
+      <SectionThree />
+    </>
+  );
+};
+
+
+export default App;
