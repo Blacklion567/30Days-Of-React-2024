0ee98cc84e1887d3bec47ebc5161273ca4387ba3
Blacklion567 <naviedaj567@gmail.com>
2024-01-21 20:59:59 +0800

    Section-06-Styling-React-Components

---
+++ src/components/Section-06-Styling-React-Components/01-starting-project/SectionSix.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
+
+export default function SectionSix() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
