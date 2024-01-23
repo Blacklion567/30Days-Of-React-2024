cbb7009fae3233acaadf59c704145c65e8aeff93
Blacklion567 <naviedaj567@gmail.com>
2024-01-23 08:34:58 +0800

    Section-06-Styling-React-Components

---
+++ src/components/Section-06-Styling-React-Components/02-Tailwind-CSS/SectionSixStyle.jsx
@@ -0,0 +1,15 @@
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
+import './index-ol.css';
+import '../../../input.css';
+
+export default function SectionSixStyle() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
